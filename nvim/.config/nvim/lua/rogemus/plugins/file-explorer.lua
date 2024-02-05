b39de731b8a48ad84aa2637eabf2dd2965597985
Kacper Rogowski <rogemus@gmail.com>
2024-02-05 20:59:38 +0100

    Add open file in GitHub

--- nvim/.config/nvim/lua/rogemus/plugins/file-explorer.lua
+++ nvim/.config/nvim/lua/rogemus/plugins/file-explorer.lua
@@ -16,6 +16,16 @@ return {
 		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
+		local get_gh_link = function(node)
+			local path = node:get_id()
+			local rel_path = vim.fn.fnamemodify(path, ":~:.")
+			local origin = vim.fn.systemlist({ "git", "config", "--get", "remote.origin.url" })
+			local repo = origin[1]:gsub("^[%a_@]+:", ""):gsub("%.git", "")
+			local branch = vim.fn.systemlist({ "git", "branch", "--show-current" })
+			local gh_domain = "https://github.com/"
+			return string.format("%s/%s/blob/%s/%s", gh_domain, repo, branch[1], rel_path)
+		end
+
 		require("neo-tree").setup({
 			-- close_if_last_window = true,
 			window = {
@@ -34,6 +44,25 @@ return {
 					["pp"] = function(state)
 						local node = state.tree:get_node()
 						require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+					end,
+					["gh"] = function(state)
+						local node = state.tree:get_node()
+						if node.type == "message" then
+							return
+						end
+						local url = get_gh_link(node)
+						local msg = "GitHub Link: " .. url
+
+						events.fire_event(events.GIT_EVENT)
+						popups.alert("github remote url", msg)
+					end,
+					["gH"] = function(state)
+						local node = state.tree:get_node()
+						if node.type == "message" then
+							return
+						end
+						local url = get_gh_link(node)
+						vim.fn.jobstart({ "open", url }, { detach = true })
 					end,
 				},
 			},
@@ -76,21 +105,17 @@ return {
 							popups.alert("git branch", result)
 						end,
 						["gch"] = function()
-							local width = vim.fn.winwidth(0) - 2
-							local row = vim.api.nvim_win_get_height(0) - 3
 							local popup_options = {
 								relative = "win",
 								position = {
-									row = row,
+									row = vim.api.nvim_win_get_height(0) - 3,
 									col = 0,
 								},
-								size = width,
+								size = vim.fn.winwidth(0) - 2,
 							}
 
 							inputs.input("Branch Name: ", "", function(msg)
-								local cmd = { "git", "checkout", msg }
-								local title = "git checkout"
-								local result = vim.fn.systemlist(cmd)
+								local result = vim.fn.systemlist({ "git", "checkout", msg })
 
 								if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "error:")) then
 									popups.alert("ERROR: git checkout", result)
@@ -98,25 +123,20 @@ return {
 								end
 
 								events.fire_event(events.GIT_EVENT)
-								popups.alert(title, result)
+								popups.alert("git checkout", result)
 							end, popup_options)
 						end,
 						["gcH"] = function()
-							local width = vim.fn.winwidth(0) - 2
-							local row = vim.api.nvim_win_get_height(0) - 3
 							local popup_options = {
 								relative = "win",
 								position = {
-									row = row,
+									row = vim.api.nvim_win_get_height(0) - 3,
 									col = 0,
 								},
-								size = width,
+								size = vim.fn.winwidth(0) - 2,
 							}
-
 							inputs.input("New Branch Name: ", "", function(msg)
-								local cmd = { "git", "checkout", "-b", msg }
-								local title = "git checkout -b"
-								local result = vim.fn.systemlist(cmd)
+								local result = vim.fn.systemlist({ "git", "checkout", "-b", msg })
 
 								if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "error:")) then
 									popups.alert("ERROR: git checkout -b", result)
@@ -129,7 +149,7 @@ return {
 								end
 
 								events.fire_event(events.GIT_EVENT)
-								popups.alert(title, result)
+								popups.alert("git checkout -b", result)
 							end, popup_options)
 						end,
 					},
