d8dd1f32d91315e231495a03b3f8b91bdaeaded1
Kacper Rogowski <rogemus@gmail.com>
2024-02-22 08:48:26 +0100

    Update config

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+
 install_ohmyzsh() {
   info "Installing Oh-My-ZSH..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -16,3 +17,4 @@ install_theme() {
   curl -L -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/rogemus.zsh-theme https://gist.githubusercontent.com/rogemus/a0db270ce9da824d53532f00754c5170/raw/962c6731c678e06c074a21b66976acd2fc8fbec3/rogemus.zsh-theme
 }
 
+"$@"
