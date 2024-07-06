d29322a21b543d616ed602a929810e6e6e1403a8
Kacper Rogowski <rogemus@gmail.com>
2024-07-06 23:00:51 +0200

    Update neo tree and deps

--- nvim/.config/nvim/lua/rogemus/plugins/file-explorer.lua
+++ nvim/.config/nvim/lua/rogemus/plugins/file-explorer.lua
@@ -27,14 +27,100 @@ return {
 		end
 
 		require("neo-tree").setup({
-			-- close_if_last_window = true,
+			commands = {
+				open_in_finder = function(state)
+					local node = state.tree:get_node()
+					if node.type == "message" then
+						return
+					end
+					if node.type == "directory" then
+						vim.fn.systemlist({ "open", node.id })
+						return
+					end
+					local dir = node:get_parent_id()
+					vim.fn.systemlist({ "open", dir })
+				end,
+				focus_parent = function(state)
+					local node = state.tree:get_node()
+					require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+				end,
+				github_file_link = function(state)
+					local node = state.tree:get_node()
+					if node.type == "message" then
+						return
+					end
+					local url = get_gh_link(node)
+					local msg = "GitHub Link: " .. url
+
+					events.fire_event(events.GIT_EVENT)
+					popups.alert("github remote url", msg)
+				end,
+				github_open_file = function(state)
+					local node = state.tree:get_node()
+					if node.type == "message" then
+						return
+					end
+					local url = get_gh_link(node)
+					vim.fn.jobstart({ "open", url }, { detach = true })
+				end,
+				filetree_toggle = function()
+					vim.cmd("Neotree reveal")
+					vim.cmd("wincmd l")
+					vim.cmd("Neotree toggle")
+				end,
+				git_pull = function()
+					local result = vim.fn.systemlist({ "git", "pull" })
+					events.fire_event(events.GIT_EVENT)
+					popups.alert("git pull", result)
+				end,
+				git_fetch = function()
+					local result = vim.fn.systemlist({ "git", "fetch" })
+					events.fire_event(events.GIT_EVENT)
+					popups.alert("git fetch", result)
+				end,
+				git_branch = function()
+					local result = vim.fn.systemlist({ "git", "branch" })
+					events.fire_event(events.GIT_EVENT)
+					popups.alert("git branch", result)
+				end,
+				git_checkout = function()
+					local popup_options = {
+						relative = "win",
+						position = {
+							row = vim.api.nvim_win_get_height(0) - 3,
+							col = 0,
+						},
+						size = vim.fn.winwidth(0) - 2,
+					}
+					inputs.input("Branch Name: ", "", function(msg)
+						local result = vim.fn.systemlist({ "git", "checkout", msg })
+
+						if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "error:")) then
+							result = vim.fn.systemlist({ "git", "checkout", "-b", msg })
+
+							if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "fatal:")) then
+								popups.alert("ERROR: git checkout -b", result)
+								return
+							end
+
+							events.fire_event(events.GIT_EVENT)
+							popups.alert("git checkout -b", result)
+						end
+
+						if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "fatal:")) then
+							popups.alert("git checkout -b", result)
+							return
+						end
+
+						events.fire_event(events.GIT_EVENT)
+						popups.alert("git checkout", result)
+					end, popup_options)
+				end,
+			},
 			window = {
 				position = "left",
 				mappings = {
-					["\\"] = function()
-						vim.cmd("wincmd l")
-						vim.cmd("Neotree toggle")
-					end,
+					["\\"] = "filetree_toggle",
 					["<space>"] = {
 						"toggle_node",
 						nowait = true,
@@ -46,41 +132,10 @@ return {
 							use_image_nvim = false,
 						},
 					},
-					["O"] = function(state)
-						local node = state.tree:get_node()
-						if node.type == "message" then
-							return
-						end
-						if node.type == "directory" then
-							vim.fn.systemlist({ "open", node.id })
-							return
-						end
-						local dir = node:get_parent_id()
-						vim.fn.systemlist({ "open", dir })
-					end,
-					["pp"] = function(state)
-						local node = state.tree:get_node()
-						require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-					end,
-					["gh"] = function(state)
-						local node = state.tree:get_node()
-						if node.type == "message" then
-							return
-						end
-						local url = get_gh_link(node)
-						local msg = "GitHub Link: " .. url
-
-						events.fire_event(events.GIT_EVENT)
-						popups.alert("github remote url", msg)
-					end,
-					["gH"] = function(state)
-						local node = state.tree:get_node()
-						if node.type == "message" then
-							return
-						end
-						local url = get_gh_link(node)
-						vim.fn.jobstart({ "open", url }, { detach = true })
-					end,
+					["O"] = "open_in_finder",
+					["pp"] = "focus_parent",
+					["gh"] = "github_file_link",
+					["gH"] = "github_open_file",
 				},
 			},
 			source_selector = {
@@ -105,73 +160,19 @@ return {
 			buffers = {
 				window = {
 					mappings = {
-						["\\"] = function()
-							vim.cmd("Neotree reveal")
-							vim.cmd("wincmd l")
-							vim.cmd("Neotree toggle")
-						end,
+						["\\"] = "filetree_toggle",
 					},
 				},
 			},
 			git_status = {
 				window = {
 					mappings = {
-						["\\"] = function()
-							vim.cmd("Neotree reveal")
-							vim.cmd("wincmd l")
-							vim.cmd("Neotree toggle")
-						end,
+						["\\"] = "filetree_toggle",
 						["gP"] = "git_push",
-						["gp"] = function()
-							local result = vim.fn.systemlist({ "git", "pull" })
-							events.fire_event(events.GIT_EVENT)
-							popups.alert("git pull", result)
-						end,
-						["gf"] = function()
-							local result = vim.fn.systemlist({ "git", "fetch" })
-							events.fire_event(events.GIT_EVENT)
-							popups.alert("git fetch", result)
-						end,
-						["gb"] = function()
-							local result = vim.fn.systemlist({ "git", "branch" })
-							events.fire_event(events.GIT_EVENT)
-							popups.alert("git branch", result)
-						end,
-						["gC"] = function()
-							local popup_options = {
-								relative = "win",
-								position = {
-									row = vim.api.nvim_win_get_height(0) - 3,
-									col = 0,
-								},
-								size = vim.fn.winwidth(0) - 2,
-							}
-							inputs.input("Branch Name: ", "", function(msg)
-								local result = vim.fn.systemlist({ "git", "checkout", msg })
-
-								if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "error:")) then
-									result = vim.fn.systemlist({ "git", "checkout", "-b", msg })
-
-									if
-										vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "fatal:"))
-									then
-										popups.alert("ERROR: git checkout -b", result)
-										return
-									end
-
-									events.fire_event(events.GIT_EVENT)
-									popups.alert("git checkout -b", result)
-								end
-
-								if vim.v.shell_error ~= 0 or (#result > 0 and vim.startswith(result[1], "fatal:")) then
-									popups.alert("git checkout -b", result)
-									return
-								end
-
-								events.fire_event(events.GIT_EVENT)
-								popups.alert("git checkout", result)
-							end, popup_options)
-						end,
+						["gp"] = "git_pull",
+						["gf"] = "git_fetch",
+						["gb"] = "git_branch",
+						["gC"] = "git_checkout",
 					},
 				},
 			},
