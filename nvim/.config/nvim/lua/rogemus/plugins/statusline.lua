45a41ce630a7015b46b962bca59a83923e85988a
Kacper Rogowski <rogemus@gmail.com>
2024-07-25 10:25:08 +0200

    Update statusline

--- nvim/.config/nvim/lua/rogemus/plugins/statusline.lua
+++ nvim/.config/nvim/lua/rogemus/plugins/statusline.lua
@@ -1,3 +1,18 @@
+local function repo_name()
+	local function is_git_repo()
+		vim.fn.system("git rev-parse --is-inside-work-tree")
+
+		return vim.v.shell_error == 0
+	end
+
+	if is_git_repo() then
+		local result = vim.fn.system("basename `git rev-parse --show-toplevel`")
+		return string.gsub(result, "%s+", "")
+	end
+
+	return ""
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -12,7 +27,7 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = {},
+				lualine_b = { repo_name },
 				lualine_c = { "filename" },
 				lualine_x = { "diagnostics", "filetype" },
 				lualine_y = { "branch", "diff" },
